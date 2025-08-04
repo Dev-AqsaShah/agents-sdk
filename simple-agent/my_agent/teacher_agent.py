bf98cb9ba6c7b48a0783a32fca5c552519d216a9
aqsashah <aqsanor665@gmail.com>
2025-08-04 15:35:44 +0500

    function tool done

--- simple-agent/my_agent/teacher_agent.py
+++ simple-agent/my_agent/teacher_agent.py
@@ -1,10 +1,17 @@
 from agents import Agent
 from my_config.gemini_config import GEMINI_MODEL
-from my_data_type.my_data_type_schema import MyData
+# from my_data_type.my_data_type_schema import MyData
+from my_tool.math_tool import plus, multiply, subtract
+
 
 gemini_agent = Agent(
     name="aqsa",
     instructions="you are a helpful assistant. give me ans in detail",
     model=GEMINI_MODEL,
-    output_type=MyData
+    # output_type=MyData
+    tools=[plus, multiply, subtract]
 )
+
+
+print(gemini_agent.name)
+print(gemini_agent.tools)
