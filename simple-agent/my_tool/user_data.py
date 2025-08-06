484262b20318d7aa3a0087c52bad2b6055c70ad3
aqsashah <aqsanor665@gmail.com>
2025-08-06 09:11:33 +0500

    .

--- simple-agent/my_tool/user_data.py
+++ simple-agent/my_tool/user_data.py
@@ -5,8 +5,18 @@ from agents import function_tool
 @function_tool
 def fetch_user_data() -> list:
     """Fetch function for user data and return list"""
+    print("Data fetch")
     url = "https://jsonplaceholder.typicode.com/users"
     res = requests.get(url)
     return res.json()
     
 # print(fetch_user_data())
+
+@function_tool
+def fetch_user_data_by_id(id:int) -> list:
+    """Fetch function for user data and return list"""
+    print("Data fetch by id")
+    url = f"https://jsonplaceholder.typicode.com/users{id}"
+    res = requests.get(url)
+    return res.json()
+    
