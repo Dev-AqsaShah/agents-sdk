387093abb6ce49ff3ac6e8c46b1b92cd06922154
aqsashah <aqsanor665@gmail.com>
2025-09-01 22:56:26 +0500

    test-prep

--- test-prep/main.py
+++ test-prep/main.py
@@ -1,1 +1,24 @@
 #  what is LLM 
+# tools nhi hote
+# data pr train hua ha
+# personal info nhi h is k passs
+# bhulaker 
+# generate answers 
+# prediction
+
+from agents import Agent, Runner
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = Agent(
+    name="Assistant",
+    instructions="You are a helpful assistant."
+)
+
+result = Runner.run_sync(
+    starting_agent=agent,
+    input="What is the capital of Pakistan?"
+)
+
+print(result.final_output)
